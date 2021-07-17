11c33072b1dc4f158683b4b35f9838a3a1780811
Jonathan Narvaez <32984331+Jonakls@users.noreply.github.com>
2021-07-17 10:13:41 -0500

    Add PlayerJoinListener in ListenersLoader

--- src/main/java/me/jonakls/souppvp/loader/ListenersLoader.java
+++ src/main/java/me/jonakls/souppvp/loader/ListenersLoader.java
@@ -3,7 +3,7 @@ package me.jonakls.souppvp.loader;
 import me.jonakls.souppvp.PluginCore;
 import me.jonakls.souppvp.SoupPvP;
 import me.jonakls.souppvp.api.Loader;
-import me.jonakls.souppvp.listener.DefaultEventListener;
+import me.jonakls.souppvp.listener.PlayerJoinListener;
 import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
@@ -20,15 +20,16 @@ public class ListenersLoader implements Loader {
     @Override
     public void load() {
         registerListeners(
-                new DefaultEventListener(pluginCore));
+                new PlayerJoinListener(pluginCore.getFilesLoader())
+        );
     }
 
     public void registerListeners(Listener... listeners){
         PluginManager pluginManager = Bukkit.getPluginManager();
-        SoupPvP souplab = pluginCore.getPlugin();
+        SoupPvP plugin = pluginCore.getPlugin();
 
         for (Listener listener : listeners){
-            pluginManager.registerEvents(listener, souplab);
+            pluginManager.registerEvents(listener, plugin);
         }
     }
 }
