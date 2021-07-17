487d6c9fef172c78fdc45223851cf1bdc40a1eec
Jonathan Narvaez <32984331+Jonakls@users.noreply.github.com>
2021-07-17 10:12:44 -0500

    Edit DefaultCommand to MainCommand

--- src/main/java/me/jonakls/souppvp/command/MainCommand.java
+++ src/main/java/me/jonakls/souppvp/command/MainCommand.java
@@ -1,13 +1,14 @@
 package me.jonakls.souppvp.command;
 
 import me.jonakls.souppvp.PluginCore;
+import me.jonakls.souppvp.manager.FileManager;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 public class MainCommand implements CommandExecutor{
 
-    private PluginCore pluginCore;
+    private final PluginCore pluginCore;
 
     public MainCommand(PluginCore pluginCore){
         this.pluginCore = pluginCore;
@@ -15,6 +16,15 @@ public class MainCommand implements CommandExecutor{
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        FileManager config = pluginCore.getFilesLoader().getConfig();
+        FileManager lang = pluginCore.getFilesLoader().getLang();
+
+        if (args.length > 0) {
+
+
+
+        }
+
         return false;
     }
 }
