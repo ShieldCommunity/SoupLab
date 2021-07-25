c28adbccda88732bd6b1909f634d9295bf31a32c
Jonathan Narvaez <32984331+Jonakls@users.noreply.github.com>
2021-07-25 15:04:51 -0500

    Added on disable method

--- src/main/java/me/jonakls/souppvp/SoupPvP.java
+++ src/main/java/me/jonakls/souppvp/SoupPvP.java
@@ -4,10 +4,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SoupPvP extends JavaPlugin {
 
+    private final PluginCore core = new PluginCore(this);
+
     @Override
     public void onEnable() {
-        PluginCore core = new PluginCore(this);
-        core.init();
+        this.core.init();
 
+    }
+
+    @Override
+    public void onDisable() {
+        this.core.getPlayerData().savePlayerData();
     }
 }
