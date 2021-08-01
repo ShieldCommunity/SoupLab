d5823d9b7922869a6f5eeeffa20f30fc2c464036
Jonathan Narvaez <32984331+Jonakls@users.noreply.github.com>
2021-08-01 01:11:06 -0500

    Edited method of database

--- src/main/java/me/jonakls/souppvp/storage/DataStorage.java
+++ src/main/java/me/jonakls/souppvp/storage/DataStorage.java
@@ -5,13 +5,15 @@ import org.bukkit.entity.Player;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class DataStorage {
 
-    private final ExecutorService EXECUTE = Executors.newCachedThreadPool();
+    private final ExecutorService EXECUTOR = Executors.newCachedThreadPool();
     private final IConnection connection;
 
     public DataStorage(IConnection connection) {
@@ -20,16 +22,11 @@ public class DataStorage {
     }
 
     private void initialize() {
-        EXECUTE.submit(() -> {
+        EXECUTOR.submit(() -> {
             try {
                 Connection con = connection.getConnection();
                 PreparedStatement statement = con.prepareStatement(
-                        "CREATE TABLE IF NOT EXISTS soup(" +
-                                "id VARCHAR(36) PRIMARY KEY, " +
-                                "name VARCHAR(60), " +
-                                "kills INTEGER, " +
-                                "deaths INTEGER, " +
-                                ")"
+                        "CREATE TABLE IF NOT EXISTS soup(id VARCHAR(36) PRIMARY KEY, name VARCHAR(60), kills INTEGER, deaths INTEGER)"
                 );
                 statement.execute();
 
@@ -41,11 +38,11 @@ public class DataStorage {
     }
 
     public void insert(PlayerCache cache, Player player) {
-        EXECUTE.submit(() -> {
+        EXECUTOR.submit(() -> {
             try {
                 Connection con = connection.getConnection();
                 PreparedStatement statement = con.prepareStatement(
-                        "REPLACE INTO soup(id, name, kills, deaths) VALUES(?, ?, ?, ?)"
+                        "REPLACE INTO soup(id, name, kills, deaths) VALUES (?, ?, ?, ?)"
                 );
                 statement.setString(1, cache.getId(player));
                 statement.setString(2, player.getName());
@@ -59,6 +56,29 @@ public class DataStorage {
             }
 
         });
+    }
+
+    public CompletableFuture<PlayerCache> select(String id) {
+        return CompletableFuture.supplyAsync(() -> {
+
+            try {
+                Connection con = connection.getConnection();
+                PreparedStatement statement = con.prepareStatement(
+                        "SELECT * FROM soup WHERE id = ?"
+                );
+
+                statement.setString(1, id);
+                ResultSet result = statement.executeQuery();
+
+                result.getString("id");
+
+
+
+            }catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }, EXECUTOR);
     }
 
 }
