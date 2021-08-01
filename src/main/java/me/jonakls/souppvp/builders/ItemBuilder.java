ed365fa28ee8e2023c7068342222102ded5a5e56
Jonathan Narvaez <32984331+Jonakls@users.noreply.github.com>
2021-08-01 01:11:39 -0500

    Added method of creating items from array string

--- src/main/java/me/jonakls/souppvp/builders/ItemBuilder.java
+++ src/main/java/me/jonakls/souppvp/builders/ItemBuilder.java
@@ -1,6 +1,7 @@
 package me.jonakls.souppvp.builders;
 
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -31,6 +32,37 @@ public class ItemBuilder {
         meta.setLore(loreItem);
         meta.setDisplayName(itemName);
         this.item.setItemMeta(meta);
+    }
+
+    public ItemBuilder(String[] strings) {
+        switch (strings.length) {
+            case 1:
+                this.item = new ItemStack(Material.valueOf(strings[0]));
+                break;
+            case 2:
+                this.item = new ItemStack(Material.valueOf(strings[0]), Integer.parseInt(strings[1]));
+                break;
+            case 3:
+                this.item = new ItemStack(Material.valueOf(strings[0]), Integer.parseInt(strings[1]));
+                ItemMeta meta = item.getItemMeta();
+                meta.setDisplayName(strings[2]);
+                this.item.setItemMeta(meta);
+                break;
+            default:
+                this.item = new ItemStack(Material.valueOf(strings[0]), Integer.parseInt(strings[1]));
+                ItemMeta meta2= item.getItemMeta();
+                meta2.setDisplayName(strings[2]);
+                for (int i = 4 ; i <= strings.length ; i++){
+                    String[] enchant = strings[i - 1].split(":");
+                    meta2.addEnchant(
+                            Enchantment.getByName(enchant[0]),
+                            Integer.parseInt(enchant[1]),
+                            true
+                    );
+                }
+                this.item.setItemMeta(meta2);
+                break;
+        }
     }
 
     public ItemStack getItem(){
